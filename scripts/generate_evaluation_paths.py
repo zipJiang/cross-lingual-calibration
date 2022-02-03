1f8b53c7039d0b33ae1a0ca267f071528496da26
zhengping <tony.jiang.zhengping@gmail.com>
2022-02-03 17:13:22 -0500

    use customized scripts to run prediction and inference

--- scripts/generate_evaluation_paths.py
+++ scripts/generate_evaluation_paths.py
@@ -21,6 +21,10 @@ def parse_args():
         '--lang', action='store', dest='lang',
         type=str, required=True, help='Which language dataset to use.'
     )
+    parser.add_argument(
+        '--return_dict', action='store_true', dest='return_dict',
+        help='Whether to return a jsonl dict.'
+    )
 
     return parser.parse_args()
 
@@ -39,7 +43,22 @@ def main():
             'fr': 'data/universal_dependency/ud-treebanks-v2.9/UD_French-PUD/fr_pud-ud-test.conllu',
             'hi': 'data/universal_dependency/ud-treebanks-v2.9/UD_Hindi-PUD/hi_pud-ud-test.conllu',
             'ru': 'data/universal_dependency/ud-treebanks-v2.9/UD_Russian-PUD/ru_pud-ud-test.conllu',
-            'zh': 'data/universal_dependency/ud-treebanks-v2.9/UD_Chinese-PUD/zh_pud-ud-test.conllu'
+            'zh': 'data/universal_dependency/ud-treebanks-v2.9/UD_Chinese-PUD/zh_pud-ud-test.conllu',
+            'calibration-train': [
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-Atis/en_atis-ud-test.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-EWT/en_ewt-ud-test.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-GUM/en_gum-ud-test.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-LinES/en_lines-ud-test.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-ParTUT/en_partut-ud-test.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-Pronouns/en_pronouns-ud-test.conllu'
+            ],
+            'calibration-dev': [
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-Atis/en_atis-ud-dev.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-EWT/en_ewt-ud-dev.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-GUM/en_gum-ud-dev.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-LinES/en_lines-ud-dev.conllu',
+                'data/universal_dependency/ud-treebanks-v2.9/UD_English-ParTUT/en_partut-ud-dev.conllu',
+            ]
         }
 
     elif args.task == 'wikiann':
@@ -51,13 +70,20 @@ def main():
             'fr': 'fr/test',
             'hi': 'hi/test',
             'ru': 'ru/test',
-            'zh': 'zh/test'
+            'zh': 'zh/test',
+            'calibration-train': 'en/validation[:3000]',
+            'calibration-dev': 'en/validation[3000:]'
         }
     else:
         raise NotImplementedError
 
     assert args.lang in config, f'requested language: {args.lang} not in {config.keys()}!'
-    print(config[args.lang])
+
+    if args.return_dict:
+        print(json.dumps({'file_path': [config[args.lang]]}))
+    else:
+        return_val = config[args.lang] if isinstance(config[args.lang], str) else ' '.join(config[args.lang])
+        print(config[args.lang])
 
 
 if __name__ == '__main__':
---
+++ scripts/predict_original_logits.py
@@ -0,0 +1,61 @@
+"""This is the function that calls the allennlp predictor
+with model from archive, and generate original logits prediction
+from the model.
+"""
+from enc_pred.predictors.calibre_predictor import SpanLabelPredictor
+from allennlp.predictors.predictor import Predictor
+
+import argparse
+import json
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        """Predicting logits for instances with trained models.
+        """
+    )
+    parser.add_argument(
+        '--archive_path', action='store', dest='archive_path',
+        type=str, required=True, help='Where to load the archive'
+    )
+    parser.add_argument(
+        '--input_path', action='store', dest='input_path',
+        type=str, required=True, help='Input path to read config.'
+    )
+    parser.add_argument(
+        '--output_path', action='store', dest='output_path',
+        type=str, required=True, help='Output path to write result to.'
+    )
+    parser.add_argument(
+        '--cuda_devices', action='store', dest='cuda_devices',
+        type=int, required=True, help='Which device to run prediction on.'
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    predictor = SpanLabelPredictor.from_path(
+        archive_path=args.archive_path,
+        predictor_name='span-label-predictor',
+        cuda_device=args.cuda_devices
+    )
+
+    with open(args.input_path, 'r', encoding='utf-8') as file_:
+        inputs = json.load(file_)
+
+    with open(args.output_path, 'w', encoding='utf-8') as file_:
+
+        for file_path in inputs['file_path']:
+            return_list = predictor.predict(
+                file_path=file_path
+            )
+
+            for item in return_list:
+                print(json.dumps(item), file=file_)
+
+
+if __name__ == '__main__':
+    main()
